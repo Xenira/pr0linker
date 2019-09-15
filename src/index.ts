d7b1b21845ee2d612ab8962e8a6528b502e44db3
Xenira <1288524+Xenira@users.noreply.github.com>
2019-09-15 19:26:21 +0200

    Added add button to user profile page

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import Events from './util/events';
 import Settings from './settings';
 import Comments from './comments';
+import User from './user';
 
 import './styles/pr0linker.scss';
 
 const settings = new Settings();
 new Comments(settings);
+new User(settings);
 Events.register();
